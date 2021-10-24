13e2dc3de181d098cb6dc2fe2dc809b5b278c197
Erika Barbosa <92835415+erika-barbosa@users.noreply.github.com>
2021-10-24 15:10:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'erika-barbosa',
+  youtube: 'channel/UCVuUKLEmjpJA55LEnkM0Z8A',
+  facebook: 'maykbrito',
+  instagram: 'er_barbosa',
+  twitter: 'maykbrito'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userProfile.href = data.html_url
+      userLogin.textContent = data.login
+      userImage.src = data.avatar_url
+    })
+}
+getGithubProfileInfos()
